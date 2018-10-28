cc674520569c2fa6df78f3899f158b8d7cae3377
The third meow <jsheaandcats@gmail.com>
2018-10-28 19:19:20 +1300

    simplify for move to CNN

--- main.py
+++ main.py
@@ -18,62 +18,33 @@ xtest, ytest = load_mnist(DATASET_PATH, 't10k')
 xtrain = keras.utils.normalize(xtrain, axis=1)
 xtest = keras.utils.normalize(xtest, axis=1)
 
-#reshape and display image with pyplot
-def show_flat_img(img, title=None):
-    if title is not None:
-        plt.title(title)
-    plt.imshow(img.reshape((28,28)))
-    plt.show()
-
-def display_mistakes(mdl):
-    prediction = mdl.predict(xtest)
-    for i in range(len(xtest)):
-        if np.argmax(prediction[i]) != ytest[i]:
-            try:
-                show_flat_img(xtest[i], ytest[i])
-            except KeyboardInterrupt:
-                print('..Closing pyplot..')
-                plt.close()
-                break
 
 #main script
 def main():
+    #create model
+    mdl = keras.models.Sequential()
 
-    if sys.argv[1] == 'load':
-        try:
-            mdl = keras.models.load_model('latest_model.model')
-        except Exception as e:
-            print(e.message, e.args)
-            print('Loading model failed, try running without "load" argument')
-            quit()
-    else:
-        #create model
-        mdl = keras.models.Sequential()
+    #add hidden layers
+    mdl.add(keras.layers.Dense(600, activation=tf.nn.tanh))
+    mdl.add(keras.layers.Dense(350, activation=tf.nn.elu))
+    mdl.add(keras.layers.Dense(350, activation=tf.nn.elu))
 
-        #add hidden layers
-        mdl.add(keras.layers.Dense(600, activation=tf.nn.tanh))
-        mdl.add(keras.layers.Dense(350, activation=tf.nn.elu))
-        mdl.add(keras.layers.Dense(350, activation=tf.nn.elu))
+    #add final layer
+    mdl.add(keras.layers.Dense(10, activation=tf.nn.sigmoid))
 
-        #add final layer
-        mdl.add(keras.layers.Dense(10, activation=tf.nn.sigmoid))
+    #complie model
+    mdl.compile(optimizer='adam', loss='sparse_categorical_crossentropy',
+            metrics=['accuracy'])
 
-        #TODO try other optimizer / loss
-        #complie model
-        mdl.compile(optimizer='adam', loss='sparse_categorical_crossentropy',
-                metrics=['accuracy'])
+    #train model
+    mdl.fit(xtrain, ytrain, epochs=3, verbose=1)
 
-        mdl.fit(xtrain, ytrain, epochs=8, verbose=1)
 
-        mdl.save('latest_model.model')
-
-    #evaluate 15 times, then average
+    #evaluate 5 times, then average
     results = []
-    for i in range(15):
+    for i in range(5):
         results.append(mdl.evaluate(xtest, ytest, verbose=1)[1])
     print(np.average(results))
-
-    display_mistakes(mdl)
 
 
 #if this is the main module, run main
